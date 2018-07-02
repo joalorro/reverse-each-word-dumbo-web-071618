dd9b0e205b91da84d4526e8c58b2462631aa5ea1
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 21:22:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 str = "Hello there, how are you?"
 
 def reverse_each_word(string)
-  string.split('/ /')
+  string = string.split('/ /')
   
-  string.collect do |word|
-    word.reverse
-  end
+ # string.collect do |word|
+#    word.reverse
+#  end
 end
 
 puts reverse_each_word(str)
